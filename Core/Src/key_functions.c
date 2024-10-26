ab8fb929459f4f7c263e358f5c843044dcfd36eb
Schmacksby <sm_github@mailbox.org>
2024-10-26 23:38:18 +0200

    structure for keymapping

--- Core/Src/key_functions.c
+++ Core/Src/key_functions.c
@@ -12,6 +12,11 @@ typedef struct {
 	uint16_t pin;
 } GPIO_Pin;
 
+typedef struct {
+	uint8_t decValue;
+	uint8_t hexValue;
+} Key_Mapping;
+
 GPIO_Pin rowsToIterate[] = {
 		{GPIOB, GPIO_PIN_3},
 		{GPIOB, GPIO_PIN_4},
@@ -37,6 +42,26 @@ GPIO_Pin colsToIterate[] = {
 		{GPIOA, GPIO_PIN_15},
 		{GPIOB, GPIO_PIN_15}
 };
+
+Key_Mapping keyMapping[] = {
+		{0, 0x29}, //Escape
+		{1, 0x1E}, //Keyboard 1
+		{2, 0x1F}, //Keyboard 2
+		{3, 0x20}, //Keyboard 3
+		{4, 0x21}, //Keyboard 4
+		{5, 0x22}, //Keyboard 5
+		{6, 0x23}, //Keyboard 6
+		{7, 0x24}, //Keyboard 7
+		{8, 0x25}, //Keyboard 8
+		{9, 0x26}, //Keyboard 9
+		{10, 0x27}, //Keyboard 0
+		{11, 0x27}, // ß
+		{12, 0x27}, // ´
+		{13, 0x2A}, //Backspace
+		//{14, 0x27}, //Keyboard 0
+		{15, 0x27}, //Keyboard 0
+};
+
 
 #define ROWS_TO_ITERATE_SIZE sizeof(rowsToIterate)/sizeof(rowsToIterate[0])
 #define COLS_TO_ITERATE_SIZE sizeof(colsToIterate)/sizeof(colsToIterate[0])
